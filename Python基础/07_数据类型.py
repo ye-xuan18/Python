81c7c455de96b8b1c271b7e77bd091c801326f45
ye-xuan18 <cyx17535736215@qq.com>
2024-02-20 19:31:49 +0800

    py

---
+++ Python基础/07_数据类型.py
@@ -0,0 +1,41 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2024/2/20 6:05 PM
+# @Author : 叶轩
+# @File : 07_数据类型
+# @Project : Python基础
+
+# 转换为整形
+
+# str-->int
+# a = '123'
+# print(type(a))
+# # 将字符串转换为整数
+# b = int(a)
+# print(type(b))
+
+# float -->int
+# a = 1.23
+# print(type(a))
+# # 如果我们将float转为整数 那么会返回的是小数点前面的数据
+# b = int(a)
+# print(b)
+# print(type(b))
+
+# boolean -->int
+# 强制类型转换为谁 就写什么方法
+# True ---> 1  False ---> 0
+# a = True
+# print(type(a))
+# b = int(a)
+# print(type(b))
+
+# a = '1.23'
+# print(type(a))
+# b = int(a)
+# print(b)
+
+
+# a = '12ab'
+# print(type(a))
+# b = int(a)
+# print(b)
