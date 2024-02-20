17a0efd65517054e94c63ac40207672464ab0c00
ye-xuan18 <cyx17535736215@qq.com>
2024-02-20 16:33:01 +0800

    js

---
+++ Python基础/03_数据类型_number_boolean_String.py
@@ -0,0 +1,41 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2024/2/20 11:50 AM
+# @Author : 叶轩
+# @File : 03_数据类型
+# @Project : Python基础
+
+# 数据类型
+# Number   数值
+# int
+# float
+# boolean  布尔
+# string   字符串
+# list     列表
+# tuple    元组
+# dict     字典
+
+# 变量类型的使用
+# Number  数值
+#     int
+money = 5000
+#     float
+money1 = 1.2
+
+# boolean 布尔
+# 流程控制语句
+# 性别的变量
+# 性别在实际的企业级开发中 使用的单词是sex gender
+# 男 True
+sex = True
+gender = False
+
+# string 字符串
+# 字符串 使用的是单引号 或者双引号
+# s = '浴火 燃不尽烧不灭只为斩诸邪'
+# s1 = '不惧一惊一险又一绝'
+
+# 单引号和双引号的嵌套
+s2 = '"嘿嘿嘿"'
+print(s2)
+s3 = "'哈哈'"
+print(s3)
