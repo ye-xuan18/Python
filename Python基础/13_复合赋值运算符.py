73f7496f0ca98d245b250a47e93bdaba2f4e06bf
ye-xuan18 <cyx17535736215@qq.com>
2024-02-21 13:21:43 +0800

    py

---
+++ Python基础/13_复合赋值运算符.py
@@ -0,0 +1,68 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2024/2/21 11:33 AM
+# @Author : 叶轩
+# @File : 13_复合赋值运算符
+# @Project : Python基础
+
+# 复合赋值运算符
+a = 1
+# # a加上一个2 打印结果
+# a = 2 + a
+# print(a)
+
+a += 2  # a = a + 2
+print(a)
+
+b = 1
+# # b乘以一个3 打印结果
+# b = b * 3
+# print(b)
+
+b *= 3  # b = b * 3
+print(b)
+
+c = 2
+#  c减去一个1 打印结果
+# c = c - 1
+# print(c)
+
+c -= 1  # c = c - 1
+print(c)
+
+d = 3
+
+#  d除以一个2 打印结果
+# d = d / 2
+# print(d)
+
+d /= 2  # d = d / 2
+print(d)
+
+e = 3
+
+# # e 整除一个2 打印结果
+# e = e // 2
+# print(e)
+
+e //= 2  # e = e // 2
+print(e)
+
+# % 取余 模余 取模
+f = 3
+# # f 对5来取余数
+# f = f % 5
+# print(f)
+
+f %= 5  # f = f % 5
+print(f)
+
+g = 5
+
+# 求下5的3次幂
+g = g ** 3
+print(g)
+
+g **= 3  # g = g ** 3
+print(g)
+
+# += -= /= //= %= **=
